ae47878625b1aa12f25456d3d3569673c571ef9e
Inev367 <irenenev24@gmail.com>
2021-06-17 16:18:06 +0000

    changed some html added pictures for home page removed collapsing cards

--- script.js
+++ script.js
@@ -15,5 +15,15 @@ function showSlides() {
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 20000); // Change image every 2 seconds
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
