9f6928d023cde21e0e359e0d6af8330812d32b94
Inev367 <irenenev24@gmail.com>
2021-07-04 11:01:57 +0000

    added back to top button

--- assets/js/script.js
+++ assets/js/script.js
@@ -97,18 +97,23 @@ function initMap() {
   }
 }
 
-//---------------------------faq
-const toggles = document.querySelectorAll('.faq-toggle')
+//--------------back to top btn
+//Get the button
+var mybutton = document.getElementById("myBtn");
 
-toggles.forEach(toggle => {
-    toggle.addEventListener('click', () => {
-        toggle.parentNode.classList.toggle
-        ('active')
-    })
-})
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
 
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
 
-//Bring in togge=le buttons (querySelectorAll)
-//loop through nodeList (forEach)
-//Add click event (addEventListener)
-//toggle the active class on the parent node (.parentNode & classList.toggle())
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
