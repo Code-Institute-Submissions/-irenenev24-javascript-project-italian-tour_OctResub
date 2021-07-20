d4b414fc080b9033c1eae9537e3047d69fca9e45
Inev367 <irenenev24@gmail.com>
2021-07-20 16:01:55 +0000

    added emailJs

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,28 @@
+  //===from Code Institute---
+  
+  function sendMail(contactForm) {
+    emailjs.send("service_hgfgk8a", "italia", {
+        "from_name": contactForm.firstname.value,
+        "lastname": contactForm.lastname.value,
+        "from_email": contactForm.email.value,
+        "phone": contactForm.phone.value,
+        "from": contactForm.from.value,
+        "to": contactForm.to.value,
+        "message": contactForm.message.value
+    })
+    .then(
+        // Alert sent if email successful
+        function(response) {
+            alert("Your email has been sent, we will be in touch as soon as possible");
+            // Page reloads after user clicks "ok" on alert box
+            // Found on Stack Overflow, linked in README
+            window.location.reload(true);
+        },
+        // Alert sent if email failed
+        function(error) {
+            alert("Your email was not sent, please try again");
+            window.location.reload(true);
+        }
+    );
+    return false;  // stops page reloading straight away
+}
