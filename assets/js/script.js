04b14f7db01e126e613b50e09c9ebddbf2408849
Inev367 <irenenev24@gmail.com>
2021-07-20 20:27:46 +0000

    worked on map code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,67 @@
+let locations = [
+    { lat:41.9028, lng:12.4964, placeName: "Sistine Chapel, Rome",
+    text: `<img src="assets/images/sistine-chapel-77.jpg" height='200' width='200'>
+    <p>The Colosseum housed gladiators who fought to the death</p>
+    <a href="https://en.wikipedia.org/wiki/Colosseum" target="_blank">Click here for more info</a>
+    <br>` },//Rome
+    { lat:41.902782, lng:12.496366 },//Sistine Chapel, Rome
+    { lat:41.9029, lng:12.4545 },//Trevi Fountain, Rome
+    { lat:41.8887, lng:12.4802 },//Cloaca Maxima, Rome
+    { lat:42.1920, lng:13.7289 },//Abruzzo
+    { lat:41.9217, lng:13.8637 },//Lake Scanno, Abruzzo
+    { lat:42.0626, lng:14.3049 },//Castle Of Roccascalegna, Abruzzo
+    { lat:41.8088, lng:13.7901 },//Parco Nazionale D’Abruzzo, Abruzzo
+    { lat:37.500000, lng:15.090278 },//sicily
+    { lat:38.1750, lng:12.7335 },//San Vito Lo Capo, Sicily
+    { lat:37.5834, lng:12.8252 },//Selinute, Sicily
+    { lat:38.1157, lng: 13.3615 }//Palermo, Siciliy
+];
+
 function initMap() {
   let map = new google.maps.Map(document.getElementById("map"), {
       zoom: 6,
       center: { lat: 41.87194, lng: 12.56738 }
   });
 
-  let labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-  let locations = [
-      { lat:41.9028, lng:12.4964, placeName: "Sistine Chapel, Rome",
-      text: `<img src="assets/images/sistine-chapel-77.jpg" height='200' width='200'>
-      <p>The Colosseum housed gladiators who fought to the death</p>
-      <a href="https://en.wikipedia.org/wiki/Colosseum" target="_blank">Click here for more info</a>
-      <br>` },//Rome
-      { lat:41.902782, lng:12.496366 },//Sistine Chapel, Rome
-      { lat:41.9029, lng:12.4545 },//Trevi Fountain, Rome
-      { lat:41.8887, lng:12.4802 },//Cloaca Maxima, Rome
-      { lat:42.1920, lng:13.7289 },//Abruzzo
-      { lat:41.9217, lng:13.8637 },//Lake Scanno, Abruzzo
-      { lat:42.0626, lng:14.3049 },//Castle Of Roccascalegna, Abruzzo
-      { lat:41.8088, lng:13.7901 },//Parco Nazionale D’Abruzzo, Abruzzo
-      { lat:37.500000, lng:15.090278 },//sicily
-      { lat:38.1750, lng:12.7335 },//San Vito Lo Capo, Sicily
-      { lat:37.5834, lng:12.8252 },//Selinute, Sicily
-      { lat:38.1157, lng: 13.3615 }//Palermo, Siciliy
+  const contentString = [{
+    placeName: "Sistine Chapel, Rome",
+    text: `<img src="assets/images/sistine-chapel-77.jpg" height='200' width='200'>
+    <p>The Colosseum housed gladiators who fought to the death</p>
+    <a href="https://en.wikipedia.org/wiki/Colosseum" target="_blank">Click here for more info</a>
+    <br>`,
+    LatLng: [{
+        lat:41.902782,
+        lng:12.496366
+    }]
+},
+              
+{
+    placeName: "Trevi Fountain, Rome",
+    text: `<img src="assets/images/trevi-fountain-small-close-up.jpg" height="200" width="200">
+    <p>Majella National Park is home to bears.</p>
+    <a href="https://en.wikipedia.org/wiki/Colosseum" target="_blank">Click here for more info</a>
+                <br>`,
+    LatLng: [{
+        lat:44.9029,
+        lng:12.4545
+    }]
+},
   ];
+const infowindow = new google.maps.InfoWindow({
+    content: contentString,
+  });
+  const marker = new google.maps.Marker({
+    position: { lat:41.902782, lng:12.496366 }, 
+    map,
+ 
+  });
+  marker.addListener("click", () => {
+    infowindow.open({
+      anchor: marker,
+      map,
+    });
+  });
+  let labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
   let markers = locations.map(function(location, i) {
       return new google.maps.Marker({
@@ -35,41 +73,40 @@ function initMap() {
   let markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
 }
 //--info windows----------
+// let markers = addMarkerInfo
 
+// function addMarkerInfo() {
+// for (let i = 0; i < locations.length; i++) {
+//     let contentString = '<div id="content"><h2>' + locations[i].placeName +
+//         '</h2><p>' + locations[i].text + '</p></div>';
 
-function addMarkerInfo() {
-for (let i = 0; i < locations.length; i++) {
-    let contentString = '<div id="content"><h2>' + locations[i].placeName +
-        '</h2><p>' + locations[i].text + '</p></div>';
+//     const marker = new google.maps.Marker({
+//         position: locations[i].LatLng[0],
+//         map: map
+//     });
 
-    const marker = new google.maps.Marker({
-        position: locations[i].LatLng[0],
-        map: map
-    });
+//     const infowindow = new google.maps.InfoWindow({
+//         content: contentString,
+//         maxWidth: 200
+//     });
 
-    const infowindow = new google.maps.InfoWindow({
-        content: contentString,
-        maxWidth: 200
-    });
+//     marker.addListener('click', function () {
+//         closeOtherInfo();
+//         infowindow.open(marker.get('map'), marker);
+//         infoObject[0] = infowindow;
+//     });
+// }
+// }
 
-    marker.addListener('click', function () {
-        closeOtherInfo();
-        infowindow.open(marker.get('map'), marker);
-        infoObject[0] = infowindow;
-    });
-}
-}
+// function closeOtherInfo() {
+// if (infoObject.length > 0) {
+//     infoObject[0].set("marker", null);
+//     infoObject[0].close();
+//     infoObject.length = 0;
+// }
+// }
 
-function closeOtherInfo() {
-if (infoObject.length > 0) {
-    infoObject[0].set("marker", null);
-    infoObject[0].close();
-    infoObject.length = 0;
-}
-}
-
-addMarkerInfo();
-
+// addMarkerInfo();
 
  //-------------------------nav
  function myFunction() {
@@ -81,12 +118,22 @@ addMarkerInfo();
   }
 }
 
+//--------------back to top btn--w3schools
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-  }
-  //-----------------attractions buttons
-  
-  
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
--- script567789.js
+++ script567789.js
@@ -41,6 +41,11 @@ let markersOnMap = [{
     }]
 },
 ];
+let addMarkerInfo = markersOnMap
+
+
+
+
 
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
@@ -125,3 +130,89 @@ function topFunction() {
   document.documentElement.scrollTop = 0;
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+let locations = [
+  { lat:41.9028, lng:12.4964, placeName: "Sistine Chapel, Rome",
+  text: `<img src="assets/images/sistine-chapel-77.jpg" height='200' width='200'>
+  <p>The Colosseum housed gladiators who fought to the death</p>
+  <a href="https://en.wikipedia.org/wiki/Colosseum" target="_blank">Click here for more info</a>
+  <br>` },//Rome
+  { lat:41.902782, lng:12.496366 },//Sistine Chapel, Rome
+  { lat:41.9029, lng:12.4545 },//Trevi Fountain, Rome
+  { lat:41.8887, lng:12.4802 },//Cloaca Maxima, Rome
+  { lat:42.1920, lng:13.7289 },//Abruzzo
+  { lat:41.9217, lng:13.8637 },//Lake Scanno, Abruzzo
+  { lat:42.0626, lng:14.3049 },//Castle Of Roccascalegna, Abruzzo
+  { lat:41.8088, lng:13.7901 },//Parco Nazionale D’Abruzzo, Abruzzo
+  { lat:37.500000, lng:15.090278 },//sicily
+  { lat:38.1750, lng:12.7335 },//San Vito Lo Capo, Sicily
+  { lat:37.5834, lng:12.8252 },//Selinute, Sicily
+  { lat:38.1157, lng: 13.3615 }//Palermo, Siciliy
+];
+
+function initMap() {
+let map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 6,
+    center: { lat: 41.87194, lng: 12.56738 }
+});
+
+const contentString = [{
+  placeName: "Sistine Chapel, Rome",
+  text: `<img src="assets/images/sistine-chapel-77.jpg" height='200' width='200'>
+  <p>The Colosseum housed gladiators who fought to the death</p>
+  <a href="https://en.wikipedia.org/wiki/Colosseum" target="_blank">Click here for more info</a>
+  <br>`,
+  LatLng: [{
+      lat:41.902782,
+      lng:12.496366
+  }]
+},
+            
+{
+  placeName: "Trevi Fountain, Rome",
+  text: `<img src="assets/images/trevi-fountain-small-close-up.jpg" height="200" width="200">
+  <p>Majella National Park is home to bears.</p>
+  <a href="https://en.wikipedia.org/wiki/Colosseum" target="_blank">Click here for more info</a>
+              <br>`,
+  LatLng: [{
+      lat:44.9029,
+      lng:12.4545
+  }]
+},
+];
+const infowindow = new google.maps.InfoWindow({
+  content: contentString,
+});
+const marker = new google.maps.Marker({
+  position: { lat:41.902782, lng:12.496366 }, 
+  map,
+  title: "Colosseum",
+});
+marker.addListener("click", () => {
+  infowindow.open({
+    anchor: marker,
+    map,
+  });
+});
+let labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+let markers = locations.map(function(location, i) {
+    return new google.maps.Marker({
+        position: location,
+        label: labels[i % labels.length]
+    });
+});
+
+let markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
+}
