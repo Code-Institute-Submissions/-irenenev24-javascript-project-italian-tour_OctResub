7131f3f866cef20b9560a5b1580cc5e22f81a420
Inev367 <irenenev24@gmail.com>
2021-07-23 19:50:08 +0000

    added to testing.md

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,28 +1,21 @@
-  //===from Code Institute---
-  
-  function sendMail(contactForm) {
-    emailjs.send("service_hgfgk8a", "italia", {
-        "from_name": contactForm.firstname.value,
-        "lastname": contactForm.lastname.value,
-        "from_email": contactForm.email.value,
-        "phone": contactForm.phone.value,
-        "from": contactForm.from.value,
-        "to": contactForm.to.value,
-        "message": contactForm.message.value
-    })
-    .then(
-        // Alert sent if email successful
-        function(response) {
-            alert("Your email has been sent, we will be in touch as soon as possible");
-            // Page reloads after user clicks "ok" on alert box
-            // Found on Stack Overflow, linked in README
-            window.location.reload(true);
+//---mix of code institute and stack overflow-------
+function sendMail(contactForm) {
+    emailjs.send("service_r19yrpd", "italia", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "from_message": contactForm.message.value
+      })
+      .then(
+        function (response) {
+          console.log("Message sent succesfully", response);
+          document.getElementById("contactForm").reset();
+          document.getElementById("onSubmitText").innerText = "Your message has been sent!";
         },
-        // Alert sent if email failed
-        function(error) {
-            alert("Your email was not sent, please try again");
-            window.location.reload(true);
+        function (error) {
+          console.log("Message not sent", error);
+          document.getElementById("onSubmitText").innerText = "Please ensure that all fields are filled in correctly";
+          document.getElementById("onSubmitText").style.color = "red";
         }
-    );
-    return false;  // stops page reloading straight away
-}
+      );
+    return false;
+  }
