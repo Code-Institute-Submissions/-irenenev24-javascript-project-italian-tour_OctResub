a34d3e33b580975c849dc828ae8b2446ebe06af3
Inev367 <irenenev24@gmail.com>
2021-10-27 18:09:50 +0000

    changed input names in the form to connect with Email.js

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,8 +1,10 @@
 //---mix of code institute and stack overflow-------
 function sendMail(contactForm) {
     emailjs.send("service_r19yrpd", "italia", {
-        "from_name": contactForm.name.value,
-        "from_email": contactForm.emailaddress.value,
+        "from_first": contactForm.first.value,
+        "from_last": contactForm.last.value,
+        "from_email": contactForm.email.value,
+        "from_number": contactForm.number.value,
         "from_message": contactForm.message.value
       })
       .then(
